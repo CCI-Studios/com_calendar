f4bc1da7557ec8886b57fce43f4849917c30d966
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-13 03:01:23 -0400

    Listbox helper for selecting calendars

---
+++ administrator/components/com_calendar/templates/helpers/listbox.php
@@ -0,0 +1,20 @@
+<?php
+
+class ComCalendarTemplateHelperListbox extends ComDefaultTemplateHelperListbox
+{
+	
+	public function calendar($config = array())
+	{
+		$config = new KConfig($config);
+		
+		$config->append(array(
+			'model' => 'calendars',
+			'name'	=> 'calendar_calendar_id',
+			'value'	=> 'id',
+			'text'	=>	'title',
+		));
+		
+		return parent::_listbox($config);
+	}
+	
+}
