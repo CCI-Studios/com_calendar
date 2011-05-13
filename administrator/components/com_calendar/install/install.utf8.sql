e28e185321885c8ec28d1405303433c2874bd92d
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-13 02:59:25 -0400

    Basic db structure

---
+++ administrator/components/com_calendar/install/install.utf8.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `#__calendar_calendars` (
+	`calendar_calendar_id` SERIAL,
+	
+	`title` VARCHAR(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__calendar_events` (
+	`calendar_event_id` SERIAL,
+	
+	`title` VARCHAR(250) NOT NULL,
+	`location` VARCHAR(250) NOT NULL,
+	`date` DATE NOT NULL,
+	`time` VARCHAR NOT NULL,
+	`intro` VARCHAR(250) NOT NULL,
+	`description` TEXT NOT NULL,
+	
+	`calendar_calendar_id` BIGINT(20) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
