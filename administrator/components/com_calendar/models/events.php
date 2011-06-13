0402a629560a39d7a4bd214397ed10ae861a307c
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-13 07:47:06 -0400

    Clean up

--- administrator/components/com_calendar/models/events.php
+++ administrator/components/com_calendar/models/events.php
@@ -25,22 +25,24 @@ class ComCalendarModelEvents extends ComDefaultModelDefault
 	
 	protected function _buildQueryWhere(KDatabaseQuery $query)
 	{
-		if (is_numeric($this->_state->day)) {
-			$query->where('DAYOFMONTH(tbl.date)', '=', $this->_state->day);
 		$state = $this->_state;
+				
+		if (is_numeric($state->day)) {
+			$query->where('DAYOFMONTH(tbl.date)', '=', $state->day);
 		}
 		if (is_numeric($this->_state->month)) {
-			$query->where('MONTH(tbl.date)', '=', $this->_state->month);
+			$query->where('MONTH(tbl.date)', '=', $state->month);
 		}
-		if (is_numeric($this->_state->year)) {
-			$query->where('YEAR(tbl.date)', '=', $this->_state->year);
+		if (is_numeric($state->year)) {
+			$query->where('YEAR(tbl.date)', '=', $state->year);
 		}
 		
-		if (is_numeric($this->_state->calendar_calendar_id)) {
-			$query->where('tbl.calendar_calendar_id', '=', $this->_state->calendar_calendar_id);
 		if ($state->future) {
 			$query->where('tbl.date', '>=', date('Y-m-d'));
 		}
+		
+		if (is_numeric($state->calendar_calendar_id)) {
+			$query->where('tbl.calendar_calendar_id', '=', $state->calendar_calendar_id);
 		}
 		
 		parent::_buildQueryWhere($query);
